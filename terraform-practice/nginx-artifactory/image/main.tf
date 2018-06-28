7ca88807267d74e21df2bfd456b96530ec8d1426
ujjwal singh <singhujjwal@gmail.com>
2018-06-28 09:58:42 +0000

    fixed the merge conflict

--- terraform-practice/nginx-artifactory/image/main.tf
+++ terraform-practice/nginx-artifactory/image/main.tf
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
 resource "docker_image" "image_id" {
     name = "${var.image}
-=======
-# Download the docker image
-resource "docker_image" "image_id" {
-    name = "${var.image}"
->>>>>>> Stashed changes
 }
