470f404e21fac8192faffcd6ba411c4d88b1a7cb
Ujjwal Singh <Ujjwal.Singh@halliburton.com>
2018-06-28 15:21:11 +0530

    pull an image

--- terraform-practice/nginx-artifactory/image/main.tf
+++ terraform-practice/nginx-artifactory/image/main.tf
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
 resource "docker_image" "image_id" {
     name = "${var.image}
+=======
+# Download the docker image
+resource "docker_image" "image_id" {
+    name = "${var.image}"
+>>>>>>> Stashed changes
 }
