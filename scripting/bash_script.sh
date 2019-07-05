435216752e07fe375106bed4bf5b1fad3e022ec9
Ujjwal Singh <ujjwal.singh@halliburton.com>
2019-07-05 14:31:34 +0530

    default value of a param

--- scripting/bash_script.sh
+++ scripting/bash_script.sh
@@ -32,3 +32,23 @@ defvalue=1
 VALUE=${2:-$defvalue}
 
 echo "The argument number two to the script in case not provided is $VALUE"
+
+
+echo "This is a way to write to an file"
+cat <<EOF > custom.conf
+server {
+  listen 80;
+  sendfile on;
+  default_type application/octet-stream;
+  gzip on;
+  gzip_http_version 1.1;
+  gzip_disable      "MSIE [1-6]\.";
+  gzip_min_length   1100;
+  gzip_vary         on;
+  gzip_proxied      expired no-cache no-store private auth;
+  gzip_types        text/plain text/css application/json application/javascript application/x-javascript text/xml application/xml application/xml+rss text/javascript;
+  gzip_comp_level   9;
+  root /usr/share/nginx/html;
+  try_files $uri $uri/ index.html;
+}
+EOF
